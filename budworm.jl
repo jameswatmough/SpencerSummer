0d4c0b78b522733bab044e92037dfd2b448974f3
spencergeddes <105650123+spencergeddes@users.noreply.github.com>
2022-06-22 00:04:34 -0300

    Add files via upload

--- budworm.jl
+++ budworm.jl
@@ -1,8 +1,6 @@
 using BifurcationKit, Plots, Setfield, Parameters, LinearAlgebra, ForwardDiff, Revise
 const BK = BifurcationKit
 
-F = (x,par) -> @. par.p + x - x^3/3
-
 function budworm!(dz, z, p, t)
     @unpack Rs, Re, Ks, Ke, P, B = p
     S, E = z
@@ -16,11 +14,11 @@ budworm(z, p) = budworm!(similar(z), z, p, 0)
 dbudworm(z,p) = ForwardDiff.jacobian(x -> budworm(x,p), z)
 jet = BK.getJet(budworm, dbudworm)
 
-opts = ContinuationPar(pMin = 8., pMax = 20., detectBifurcation = 3)
+opts = ContinuationPar(dsmin = 1.e-4, dsmax = 1.e-2, ds = -1.e-4, pMin = -17., pMax = 20., detectBifurcation = 3)
+#in order to reverse the direction of the continuation, the sign of ds must be flipped.
 
-params = (Rs = 100., Re = 95., Ks = 1.1, Ke = 1.2, P = 1., B = 8.)
-#what are some reasonable initial parameters for this system? Find better ones than these.
+params = (Rs = 100., Re = 95., Ks = 1.1, Ke = 1.2, P = 1., B = 1.)
 
-br, = continuation(budworm, dbudworm, [0.7,0.7], params, (@lens _.B), opts; printSolution = (x,p) -> (S = x[1], E = x[2]))
+br, = continuation(budworm, dbudworm, [0.1,0.1], params, (@lens _.B), opts; printSolution = (x,p) -> (S = x[1], E = x[2]))
 
 plot(br)

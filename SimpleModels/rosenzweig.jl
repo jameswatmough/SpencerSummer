5667e5e4a2343ff4a3c9670474c3cee67a82f44c
James Watmough <watmough@unb.ca>
2022-07-13 13:39:02 -0300

    use bifurcationdiagarm() for rosenzweig model

--- SimpleModels/rosenzweig.jl
+++ SimpleModels/rosenzweig.jl
@@ -1,25 +1,49 @@
-using DifferentialEquations, Plots
+using BifurcationKit, Plots, Setfield, Parameters, LinearAlgebra, ForwardDiff, Revise
+const BK = BifurcationKit
+
+# sup norm
+norminf(x) = norm(x, Inf)
 
 function rosenzweig!(dx,x,p,t)
-	b,d,K,a,w,c,m = p
-	dx[1] = (b-d)*x[1]*(1 - x[1]/K) - a*w*x[1]*y[1]/(w+x[1])
-	dx[2] = c*a*w*x[1]*y[1]/(w+x[1]) - m*y[1]
+	@unpack μ,γ,κ = p
+	dx[1] = x[1]*(1 - x[1]/κ) - x[1]*x[2]/(1.0+x[1])
+	dx[2] = μ*x[2]*(γ*x[1]/(1.0+x[1]) - 1.0)
+	return(dx)
 end
 
-# sample parameter values 
-b=2
-d=1
-K=1000
-a=0.005
-w=400
-c=2
-m=2
+rosenzweig(z, p) = rosenzweig!(similar(z), z, p, 0)
 
-p = [b,d,K,a,w,c,m]
-x0 = [500.,500.]
+# sample parameter values 
+par_base = (μ=1., γ=2.0, κ=0.5)
+
+x0 = [par_base.κ,0.]
 tspan = (0.,100.)
 
-# the equilibrium prey population is
-Nstar = m*w/(c*a*w-m)  
-# a hopf bifurcation occurs as K is increased through
-Khopf = 2*Nstar+w
+jet = BK.getJet(rosenzweig; matrixfree=false)
+
+# continuation options
+opts_br = ContinuationPar(pMin = 0.0, pMax = 5.,
+	# parameters to have a smooth result
+	ds = 0.04, dsmax = 0.05,
+	# this is to detect bifurcation points precisely with bisection
+	detectBifurcation = 3,
+	# Optional: bisection options for locating bifurcations
+	nInversion = 8, maxBisectionSteps = 25, nev = 3)
+
+# continuation of equilibria
+br, = continuation(jet[1], jet[2], x0, par_base, (@lens _.κ), opts_br;
+	recordFromSolution = (x, p) -> (prey = x[1], predator = x[2]),
+	tangentAlgo = BorderedPred(),
+	plot = true, normC = norminf)
+
+diagram = bifurcationdiagram(jet...,
+	# initial point and parameter
+	x0, par_base,
+	# specify the continuation parameter
+	(@lens _.κ),
+	# very important parameter. This specifies the maximum amount of recursion
+	# when computing the bifurcation diagram. It means we allow computing branches of branches
+	# at most in the present case.
+	2,
+	(args...) -> setproperties(opts_br; pMin = 0.0, pMax = 5., ds = 0.04, dsmax = 0.05, nInversion = 8, detectBifurcation = 3, dsminBisection =1e-18, maxBisectionSteps=20);
+	recordFromSolution = (x, p) -> (predator = x[2], prey = x[1]))
